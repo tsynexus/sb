9c0e69af78d2eb6ab0b485de128406f8f69d1dea
tsynexus <tsynexus@gmail.com>
2024-04-16 17:18:43 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -39,11 +39,11 @@ def get_physical_location(address):
         )  # 这里的路径需要指向你自己的数据库文件
         response = reader.city(ip_address)
         country = response.country.name
-        city = response.city.name
+        # city = response.city.name
         # return f"{country}_{city}"
         return f"{country}"
     except geoip2.errors.AddressNotFoundError as e:
-        print(f"Error: {e}")
+        # print(f"Error: {e}")
         return "Unknown"
 
 
@@ -85,9 +85,13 @@ def process_clash(data, index):
                 security = "tls"
             location = get_physical_location(server)
             name = f"{location}_vless_{index}"
-            vless_meta = f"vless://{uuid}@{server}:{port}?security={security}&allowInsecure{insecure}&flow={flow}&type={network}&fp={fp}&pbk={publicKey}&sid={short_id}&sni={sni}&serviceName={grpc_serviceName}&path={ws_path}&host={ws_headers_host}#{name}"
-
-            merged_proxies.append(vless_meta)
+            vless_meta2 = f"vless://{uuid}@{server}:{port}?security={security}&allowInsecure{insecure}&flow={flow}&type={network}&fp={fp}&pbk={publicKey}&sid={short_id}&sni={sni}&serviceName={grpc_serviceName}&path={ws_path}&host={ws_headers_host}"
+            #判断列表中是否已包含
+            result = any(vless_meta2 in word for word in merged_proxies)
+            #如果没有存在，写入
+            if not result:
+                vless_meta = f"{vless_meta2}#{name}"
+                merged_proxies.append(vless_meta)
 
         if proxy["type"] == "vmess":
             server = proxy.get("server", "")
@@ -101,16 +105,23 @@ def process_clash(data, index):
                 security = "none"
             elif tls == 1:
                 security = "tls"
+            insecure = int(proxy.get("skip-cert-verify", 0))
             sni = proxy.get("servername", "")
+            fp = proxy.get("fingerprint", "")
             ws_path = proxy.get("ws-opts", {}).get("path", "")
             ws_headers_host = (
                 proxy.get("ws-opts", {}).get("headers", {}).get("Host", "")
             )
-            location = get_physical_location(server)
-            name = f"{location}_vmess_{index}"
-            vmess_meta = f"vmess://{uuid}@{server}:{port}?security={security}&allowInsecure{insecure}&type={network}&fp={fp}&sni={sni}&path={ws_path}&host={ws_headers_host}#{name}"
+            # location = get_physical_location(server)
 
-            merged_proxies.append(vmess_meta)
+            name = f"_vmess_{index}"
+            vmess_meta2 = f"vmess://{uuid}@{server}:{port}?security={security}&allowInsecure={insecure}&alter_id={alterId}&sni={sni}&type={network}&fp={fp}&path={ws_path}&host={ws_headers_host}"
+            #判断列表中是否已包含
+            result = any(vmess_meta2 in word for word in merged_proxies)
+            #如果没有存在，写入
+            if not result:
+                vmess_meta = f"{vmess_meta2}#{name}"
+                merged_proxies.append(vmess_meta)
 
         elif proxy["type"] == "tuic":
             server = proxy.get("server", "")
@@ -129,8 +140,13 @@ def process_clash(data, index):
             location = get_physical_location(server)
             name = f"{location}_tuic_{index}"
             # tuic_meta_neko = f"tuic://{server}:{port}?uuid={uuid}&version=5&password={password}&insecure={insecure}&alpn={alpn}&mode={udp_relay_mode}"
-            tuic_meta = f"tuic://{uuid}:{password}@{server}:{port}?sni={sni}&congestion_control={congestion}&udp_relay_mode={udp_relay_mode}&alpn={alpn}&allow_insecure={insecure}#{name}"
-            merged_proxies.append(tuic_meta)
+            tuic_meta2 = f"tuic://{uuid}:{password}@{server}:{port}?sni={sni}&congestion_control={congestion}&udp_relay_mode={udp_relay_mode}&alpn={alpn}&allow_insecure={insecure}"
+            #判断列表中是否已包含
+            result = any(tuic_meta2 in word for word in merged_proxies)
+            #如果没有存在，写入
+            if not result:
+                tuic_meta = f"{tuic_meta2}#{name}"
+                merged_proxies.append(tuic_meta)
 
         elif proxy["type"] == "hysteria2":
             server = proxy.get("server", "")
@@ -142,8 +158,13 @@ def process_clash(data, index):
             insecure = int(proxy.get("skip-cert-verify", 0))
             location = get_physical_location(server)
             name = f"{location}_hy2_{index}"
-            hy2_meta = f"hysteria2://{auth}@{server}:{port}?insecure={insecure}&sni={sni}&obfs={obfs}&obfs-password={obfs_password}#{name}"
-            merged_proxies.append(hy2_meta)
+            hy2_meta2 = f"hysteria2://{auth}@{server}:{port}?insecure={insecure}&sni={sni}&obfs={obfs}&obfs-password={obfs_password}"
+            #判断列表中是否已包含
+            result = any(hy2_meta2 in word for word in merged_proxies)
+            #如果没有存在，写入
+            if not result:
+                hy2_meta = f"{hy2_meta2}#{name}"
+                merged_proxies.append(hy2_meta)
 
         elif proxy["type"] == "hysteria":
             server = proxy.get("server", "")
@@ -165,80 +186,85 @@ def process_clash(data, index):
             # 生成URL
             location = get_physical_location(server)
             name = f"{location}_hy_{index}"
-            hysteria_meta = f"hysteria://{server}:{port}?peer={sni}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&mport={ports}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}#{name}"
-            merged_proxies.append(hysteria_meta)
+            hysteria_meta2 = f"hysteria://{server}:{port}?peer={sni}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&mport={ports}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}"
+            #判断列表中是否已包含
+            result = any(hysteria_meta2 in word for word in merged_proxies)
+            #如果没有存在，写入
+            if not result:
+                hysteria_meta = f"{hysteria_meta2}#{name}"
+                merged_proxies.append(hysteria_meta)
 
-        elif proxy["type"] == "ssr":
-            server = proxy.get("server", "")
-            port = int(proxy.get("port", 443))
-            password = proxy.get("password", "")
-            password = base64.b64encode(password.encode()).decode()
-            cipher = proxy.get("cipher", "")
-            obfs = proxy.get("obfs", "")
-            protocol = proxy.get("protocol", "")
-            protocol_param = proxy.get("protocol-param", "")
-            protocol_param = base64.b64encode(protocol_param.encode()).decode()
-            obfs_param = proxy.get("obfs-param", "")
-            obfs_param = base64.b64encode(obfs_param.encode()).decode()
-            # 生成URL
-            ssr_source = f"{server}:{port}:{protocol}:{cipher}:{obfs}:{password}/?obfsparam={obfs_param}&protoparam={protocol_param}&remarks=ssr_meta_{index}&protoparam{protocol_param}=&obfsparam={obfs_param}"
-            ssr_source = base64.b64encode(ssr_source.encode()).decode()
-            ssr_meta = f"ssr://{ssr_source}"
-            merged_proxies.append(ssr_meta)
-        # 目前仅支持最原始版本ss，无插件支持
-        elif proxy["type"] == "sstest":
-            server = proxy.get("server", "")
-            port = int(proxy.get("port", 443))
-            password = proxy.get("password", "")
-            cipher = proxy.get("cipher", "")
-            # 生成URL
-            ss_source = f"{cipher}:{password}@{server}:{port}"
+        # elif proxy["type"] == "ssr":
+        #     server = proxy.get("server", "")
+        #     port = int(proxy.get("port", 443))
+        #     password = proxy.get("password", "")
+        #     password = base64.b64encode(password.encode()).decode()
+        #     cipher = proxy.get("cipher", "")
+        #     obfs = proxy.get("obfs", "")
+        #     protocol = proxy.get("protocol", "")
+        #     protocol_param = proxy.get("protocol-param", "")
+        #     protocol_param = base64.b64encode(protocol_param.encode()).decode()
+        #     obfs_param = proxy.get("obfs-param", "")
+        #     obfs_param = base64.b64encode(obfs_param.encode()).decode()
+        #     # 生成URL
+        #     ssr_source = f"{server}:{port}:{protocol}:{cipher}:{obfs}:{password}/?obfsparam={obfs_param}&protoparam={protocol_param}&remarks=ssr_meta_{index}&protoparam{protocol_param}=&obfsparam={obfs_param}"
+        #     ssr_source = base64.b64encode(ssr_source.encode()).decode()
+        #     ssr_meta = f"ssr://{ssr_source}"
+        #     merged_proxies.append(ssr_meta)
+        # # 目前仅支持最原始版本ss，无插件支持
+        # elif proxy["type"] == "sstest":
+        #     server = proxy.get("server", "")
+        #     port = int(proxy.get("port", 443))
+        #     password = proxy.get("password", "")
+        #     cipher = proxy.get("cipher", "")
+        #     # 生成URL
+        #     ss_source = f"{cipher}:{password}@{server}:{port}"
+        #
+        #     ss_source = base64.b64encode(ss_source.encode()).decode()
+        #     ss_meta = f"ss://{ss_source}"
+        #     merged_proxies.append(ss_meta)
 
-            ss_source = base64.b64encode(ss_source.encode()).decode()
-            ss_meta = f"ss://{ss_source}"
-            merged_proxies.append(ss_meta)
 
-
-def process_naive(data, index):
-    try:
-        json_data = json.loads(data)
-
-        proxy_str = json_data["proxy"]
-        # proxy_str = proxy_str.replace("https://", "")
-        naiveproxy = base64.b64encode(proxy_str.encode()).decode()
-        merged_proxies.append(naiveproxy)
-
-    except Exception as e:
-        logging.error(f"Error processing naive data for index {index}: {e}")
-
+# def process_naive(data, index):
+#     try:
+#         json_data = json.loads(data)
+#
+#         proxy_str = json_data["proxy"]
+#         # proxy_str = proxy_str.replace("https://", "")
+#         naiveproxy = base64.b64encode(proxy_str.encode()).decode()
+#         merged_proxies.append(proxy_str)
+#
+#     except Exception as e:
+#         logging.error(f"Error processing naive data for index {index}: {e}")
+#
 
 # 处理sing-box节点，待办
-def process_sb(data, index):
-    try:
-        json_data = json.loads(data)
-        # 处理 shadowtls 数据
-        server = json_data["outbounds"][1].get("server", "")
-        server_port = json_data["outbounds"][1].get("server_port", "")
-        method = json_data["outbounds"][0].get("method", "")
-        password = json_data["outbounds"][0].get("password", "")
-        version = int(json_data["outbounds"][1].get("version", 0))
-        host = json_data["outbounds"][1]["tls"].get("server_name", "")
-        shadowtls_password = json_data["outbounds"][1].get("password", "")
-
-        ss = f"{method}:{password}@{server}:{server_port}"
-        shadowtls = f'{{"version": "{version}", "host": "{host}","password":{shadowtls_password}}}'
-        shadowtls_proxy = (
-            "ss://"
-            + base64.b64encode(ss.encode()).decode()
-            + "?shadow-tls="
-            + base64.b64encode(shadowtls.encode()).decode()
-            + f"#shadowtls{index}"
-        )
-
-        merged_proxies.append(shadowtls_proxy)
-
-    except Exception as e:
-        logging.error(f"Error processing shadowtls data for index {index}: {e}")
+# def process_sb(data, index):
+#     try:
+#         json_data = json.loads(data)
+#         # 处理 shadowtls 数据
+#         server = json_data["outbounds"][1].get("server", "")
+#         server_port = json_data["outbounds"][1].get("server_port", "")
+#         method = json_data["outbounds"][0].get("method", "")
+#         password = json_data["outbounds"][0].get("password", "")
+#         version = int(json_data["outbounds"][1].get("version", 0))
+#         host = json_data["outbounds"][1]["tls"].get("server_name", "")
+#         shadowtls_password = json_data["outbounds"][1].get("password", "")
+#
+#         ss = f"{method}:{password}@{server}:{server_port}"
+#         shadowtls = f'{{"version": "{version}", "host": "{host}","password":{shadowtls_password}}}'
+#         shadowtls_proxy = (
+#             "ss://"
+#             + base64.b64encode(ss.encode()).decode()
+#             + "?shadow-tls="
+#             + base64.b64encode(shadowtls.encode()).decode()
+#             + f"#shadowtls{index}"
+#         )
+#         print(shadowtls_proxy)
+#         merged_proxies.append(shadowtls_proxy)
+#
+#     except Exception as e:
+#         logging.error(f"Error processing shadowtls data for index {index}: {e}")
 
 
 # hysteria
@@ -260,9 +286,13 @@ def process_hysteria(data, index):
         # 生成URL
         location = get_physical_location(server)
         name = f"{location}_hysteria_{index}"
-        hysteria = f"hysteria://{server}?peer={server_name}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}#{name}"
-        merged_proxies.append(hysteria)
-
+        hysteria_2 = f"hysteria://{server}?peer={server_name}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}"
+        # 判断列表中是否已包含
+        result = any(hysteria_2 in word for word in merged_proxies)
+        # 如果没有存在，写入
+        if not result:
+            hysteria = f"{hysteria_2}#{name}"
+            merged_proxies.append(hysteria)
     except Exception as e:
         logging.error(f"Error processing hysteria data for index {index}: {e}")
 
@@ -280,9 +310,14 @@ def process_hysteria2(data, index):
         # 生成URL
         location = get_physical_location(server)
         name = f"{location}_hysteria2_{index}"
-        hysteria2 = f"hysteria2://{auth}@{server}?insecure={insecure}&sni={sni}#{name}"
+        hysteria2_2 = f"hysteria2://{auth}@{server}?insecure={insecure}&sni={sni}"
+        # 判断列表中是否已包含
+        result = any(hysteria2_2 in word for word in merged_proxies)
+        # 如果没有存在，写入
+        if not result:
+            hysteria2 = f"{hysteria2_2}#{name}"
+            merged_proxies.append(hysteria2)
 
-        merged_proxies.append(hysteria2)
     except Exception as e:
         logging.error(f"Error processing hysteria2 data for index {index}: {e}")
 
@@ -332,23 +367,28 @@ def process_xray(data, index):
             ws_headers_host = ws_settings.get("headers", {}).get("Host", "")
             location = get_physical_location(server)
             name = f"{location}_vless_{index}"
-            xray_proxy = f"vless://{uuid}@{server}:{port}?security={security}&allowInsecure={insecure}&flow={flow}&type={network}&fp={fp}&pbk={publicKey}&sid={short_id}&sni={sni}&serviceName={grpc_serviceName}&path={ws_path}&host={ws_headers_host}#{name}"
-
+            xray_proxy2 = f"vless://{uuid}@{server}:{port}?security={security}&allowInsecure={insecure}&flow={flow}&type={network}&fp={fp}&pbk={publicKey}&sid={short_id}&sni={sni}&serviceName={grpc_serviceName}&path={ws_path}&host={ws_headers_host}"
+            # 判断列表中是否已包含
+            result = any(xray_proxy2 in word for word in merged_proxies)
+            # 如果没有存在，写入
+            if not result:
+                xray_proxy = f"{xray_proxy2}#{name}"
+                merged_proxies.append(xray_proxy)
             # 将当前proxy字典添加到所有proxies列表中
-            merged_proxies.append(xray_proxy)
+
         # 不支持插件
-        if protocol == "shadowsocks":
-            server = json_data["outbounds"][0]["settings"]["servers"]["address"]
-            method = json_data["outbounds"][0]["settings"]["servers"]["method"]
-            password = json_data["outbounds"][0]["settings"]["servers"]["password"]
-            port = json_data["outbounds"][0]["settings"]["servers"]["port"]
-            # 生成URL
-            ss_source = f"{method}:{password}@{server}:{port}"
-            ss_source = base64.b64encode(ss_source.encode()).decode()
-            xray_proxy = f"ss://{ss_source}"
-
-            # 将当前proxy字典添加到所有proxies列表中
-            merged_proxies.append(xray_proxy)
+        # if protocol == "shadowsocks":
+        #     server = json_data["outbounds"][0]["settings"]["servers"]["address"]
+        #     method = json_data["outbounds"][0]["settings"]["servers"]["method"]
+        #     password = json_data["outbounds"][0]["settings"]["servers"]["password"]
+        #     port = json_data["outbounds"][0]["settings"]["servers"]["port"]
+        #     # 生成URL
+        #     ss_source = f"{method}:{password}@{server}:{port}"
+        #     ss_source = base64.b64encode(ss_source.encode()).decode()
+        #     xray_proxy = f"ss://{ss_source}"
+        #
+        #     # 将当前proxy字典添加到所有proxies列表中
+        #     merged_proxies.append(xray_proxy)
     except Exception as e:
         logging.error(f"Error processing xray data for index {index}: {e}")
 
@@ -363,7 +403,7 @@ process_urls("./urls/clash_urls.txt", process_clash)
 # process_urls('./urls/sb_urls.txt', process_sb)
 
 # 处理 naive URLs
-process_urls("./urls/naiverproxy_urls.txt", process_naive)
+# process_urls("./urls/naiverproxy_urls.txt", process_naive)
 
 # 处理 hysteria URLs
 process_urls("./urls/hysteria_urls.txt", process_hysteria)
@@ -374,6 +414,7 @@ process_urls("./urls/hysteria2_urls.txt", process_hysteria2)
 # 处理 xray URLs
 process_urls("./urls/xray_urls.txt", process_xray)
 
+# print(merged_proxies)
 # 将结果写入文件
 merged_content = "\n".join(merged_proxies)
 
@@ -383,6 +424,6 @@ try:
     with open("./sub/base64.txt", "w") as encoded_file:
         encoded_file.write(encoded_content)
 
-    print("Content successfully encoded and written to base64.txt.")
+    print("ok to base64.txt.")
 except Exception as e:
-    print(f"Error encoding and writing to file: {e}")
+    print(f"Error to file: {e}")
